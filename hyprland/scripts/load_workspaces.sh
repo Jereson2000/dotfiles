c3503d45bbf8878fd38c6a7b6b8610e903ad9fbc
jereson2000 <jere.lotvonen@gmail.com>
2025-08-26 11:42:25 +0300

    add script to load all the workspaces

---
+++ hyprland/scripts/load_workspaces.sh
@@ -0,0 +1,11 @@
+hyprctl notify -1 1000 "rgb(4257f5)" "Loading workspaces..."
+
+sleep 1
+
+for i in $(seq 6 -1 1);
+do
+	hyprctl dispatch workspace "$i"
+done
+
+hyprctl notify -1 3000 "rgb(42f54e)" "Workspaces loaded."
+
