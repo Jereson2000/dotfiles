024526263578ae8a46a7c0f8ba9d89af3f0b7f9c
Jere Lotvonen <jere.lotvonen@gmail.com>
2025-11-01 11:56:41 +0200

    refactor keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -5,7 +5,7 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -24,4 +24,4 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<leader>n', '<Cmd>Neotree toggle position=right<CR>')
+vim.keymap.set('n', '<leader>n', '<Cmd>Neotree toggle position=right<CR>', { desc = 'Toggle Neotree' })
